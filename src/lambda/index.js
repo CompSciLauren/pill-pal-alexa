58461c62614a6f24ff565c0844784780eab98158
Lauren Stephenson <compscilauren@gmail.com>
2020-04-13 15:47:07 -0500

    Update most functions to reflect database updates

--- src/lambda/index.js
+++ src/lambda/index.js
@@ -99,7 +99,7 @@ function onSessionEnded(sessionEndedRequest, session) {
 function giveName(intent, session, callback) {
     http.get({
         host: 'pillpal-app.de',
-        path: '/User/email@gmail.com',
+        path: '/User/0',
     }, function(res) {
         res.setEncoding('utf8');
         // Continuously update stream with data
@@ -115,7 +115,7 @@ function giveName(intent, session, callback) {
                 // callback(parsed.MRData);
                 
                 callback(session.attributes,
-                    buildSpeechletResponseWithoutCard("You are currently managing the account that belongs to " + parsed.Name + ".", "", "true"));
+                    buildSpeechletResponseWithoutCard("You are currently managing the account that belongs to " + parsed[0].First_Name + " " + parsed[0].Last_Name + ".", "", "true"));
                 
                 //return parsed.MRData;
             } catch (err) {
@@ -133,7 +133,7 @@ function giveName(intent, session, callback) {
 function giveCurrentPills(intent, session, callback) {
     http.get({
         host: 'pillpal-app.de',
-        path: '/Takes/email@gmail.com',
+        path: '/Takes/0',
     }, function(res) {
         res.setEncoding('utf8');
         // Continuously update stream with data
@@ -154,11 +154,11 @@ function giveCurrentPills(intent, session, callback) {
                     {
                         if (i == 0)
                         {
-                             listOfMeds += parsed[i].Medication_Name;   
+                             listOfMeds += parsed[i].Display_Name;   
                         }
                         else
                         {
-                            listOfMeds += " and " + parsed[i].Medication_Name;  
+                            listOfMeds += " and " + parsed[i].Display_Name;  
                         }
                     }
                     callback(session.attributes,
@@ -187,10 +187,11 @@ function addPill(intent, session, callback) {
     const request = require('request');
     
     var postData = {
-      'User_Email' : 'email@gmail.com',
-      'Medication_Name': intent.slots.Pill.value,
+      'User_ID' : '0',
+      'Medication_ID': 'error',
       'Amount_Prescribed': intent.slots.Amount_Prescribed.value,
-      'Refills': intent.slots.Refills.value
+      'Refills': intent.slots.Refills.value,
+      'Display_Name': intent.slots.Pill.value
     };
     
     request.post('https://pillpal-app.de/Takes', {json: postData}, (error, response, body) => {
@@ -199,15 +200,15 @@ function addPill(intent, session, callback) {
         console.log('RESPONSE: ' + JSON.stringify(response));
         if (JSON.stringify(body.message).includes('ER_DUP_ENTRY'))
         {
-            callback(session.attributes, buildSpeechletResponseWithoutCard('You already have ' + postData.Medication_Name + ' as a current pill.', "", "true"));
+            callback(session.attributes, buildSpeechletResponseWithoutCard('You already have ' + postData.Display_Name + ' as a current pill.', "", "true"));
         }
         else if (JSON.stringify(body.message).includes('ER_NO_REFERENCED_ROW_2'))
         {
-            callback(session.attributes, buildSpeechletResponseWithoutCard('Sorry, ' + postData.Medication_Name + ' is not currently registered with PillPal as an available medication. Go to the Help page in Settings on the PillPal app for more information or to report a problem.', "", "true"));
+            callback(session.attributes, buildSpeechletResponseWithoutCard('Sorry, ' + postData.Display_Name + ' is not currently registered with PillPal as an available medication. Go to the Help page in Settings on the PillPal app for more information or to report a problem.', "", "true"));
         }
         else
         {
-            callback(session.attributes, buildSpeechletResponseWithoutCard('Okay, I\'ve added ' + postData.Medication_Name + ' as a current pill. You need to take ' + postData.Amount_Prescribed + ' each day and you have ' + postData.Refills + ' refills remaining.', "", "true"));   
+            callback(session.attributes, buildSpeechletResponseWithoutCard('Okay, I\'ve added ' + postData.Display_Name + ' as a current pill. You need to take ' + postData.Amount_Prescribed + ' each day and you have ' + postData.Refills + ' refills remaining.', "", "true"));   
         }
     });
 }
@@ -216,23 +217,23 @@ function removeOnePill(intent, session, callback) {
     const request = require('request');
     
     var postData = {
-      'User_Email' : 'email@gmail.com',
-      'Medication_Name': intent.slots.Pill.value
+      'User_ID' : '0',
+      'Display_Name': intent.slots.Pill.value
     };
     
-    request.delete('https://pillpal-app.de/Takes/email@gmail.com/' + postData.Medication_Name, (error, response, body) => {
+    request.delete('https://pillpal-app.de/Takes/0/' + postData.Display_Name, (error, response, body) => {
         console.log('ERROR: ' + error);
         console.log('BODY: ' + JSON.stringify(body));
         console.log('RESPONSE: ' + JSON.stringify(response));
         if (JSON.stringify(response.statusCode) == 404)
         {
             callback(session.attributes,
-                buildSpeechletResponseWithoutCard(postData.Medication_Name + ' is not one of your current pills.', "", "true"));
+                buildSpeechletResponseWithoutCard(postData.Display_Name + ' is not one of your current pills.', "", "true"));
         }
         else
         {
             callback(session.attributes,
-                buildSpeechletResponseWithoutCard('Okay, I\'ve deleted ' + postData.Medication_Name + ' from your current pills.', "", "true"));   
+                buildSpeechletResponseWithoutCard('Okay, I\'ve deleted ' + postData.Display_Name + ' from your current pills.', "", "true"));   
         }
     });
 }
@@ -240,7 +241,7 @@ function removeOnePill(intent, session, callback) {
 function removeAllPills(intent, session, callback) {
     const request = require('request');
     
-    request.delete('https://pillpal-app.de/Takes/email@gmail.com', (error, response, body) => {
+    request.delete('https://pillpal-app.de/Takes/0', (error, response, body) => {
         console.log('ERROR: ' + error);
         console.log('BODY: ' + JSON.stringify(body));
         console.log('RESPONSE: ' + JSON.stringify(response));
